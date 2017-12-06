fca07f3762345485e6a0ab9e98aff1a7e97bfd40
CharlesFeng <fdfengjunjie@126.com>
2017-12-06 21:22:03 +0800

    Express 框架初始化 + sqlite

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var sqlite = require('sqlite3');
+var db = new sqlite.Database('./MarvelNote.sqlite');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  db.all("select * from user", function (err, res) {
+    if (err) console.log(err);
+    else console.log(JSON.stringify(res));
+  });
+
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
