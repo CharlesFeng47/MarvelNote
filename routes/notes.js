a98350ee370d55d981a13a2ff42c655ece7efff6
CharlesFeng <fdfengjunjie@126.com>
2017-12-08 13:38:38 +0800

    ADD：对笔记的增删操作

--- routes/notes.js
+++ routes/notes.js
@@ -5,14 +5,48 @@ var sqlite = require('sqlite3');
 var db = new sqlite.Database('./MarvelNote.sqlite');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(request, response, next) {
 
-  db.all("select * from user", function (err, res) {
+  db.all("select * from note", function (err, res) {
     if (err) console.log(err);
     else console.log(JSON.stringify(res));
   });
 
-  res.render('notes', { title: 'Express', aa: '<h4>aa</h4>' });
+  response.render('notes');
+});
+
+/* share a note */
+router.post('/share_note', function(request, response, next) {
+
+  var request_body = request.body;
+  var sql = "update note set 'is_public' = '1' where note_id = '" + request_body.note_id + "'";
+
+  db.all(sql, function (err, res) {
+    if (err) {
+      console.log(err);
+      response.render('error')
+    } else {
+      console.log(JSON.stringify(res));
+      response.send('share success');
+    }
+  });
+});
+
+/* delete a note */
+router.post('/delete_note', function(request, response, next) {
+
+  var request_body = request.body;
+  var sql = "delete from note where note_id = '" + request_body.note_id + "'";
+
+  db.all(sql, function (err, res) {
+    if (err) {
+      console.log(err);
+      response.render('error')
+    } else {
+      console.log(JSON.stringify(res));
+      response.send('delete success');
+    }
+  });
 });
 
 module.exports = router;
