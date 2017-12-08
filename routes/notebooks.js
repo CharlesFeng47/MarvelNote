991610dd47f42eea0f58b3ec98c3d6d8ac5bf8db
CharlesFeng <fdfengjunjie@126.com>
2017-12-08 13:21:10 +0800

    ADD：对笔记本的增删操作

--- routes/notebooks.js
+++ routes/notebooks.js
@@ -5,14 +5,49 @@ var sqlite = require('sqlite3');
 var db = new sqlite.Database('./MarvelNote.sqlite');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (request, response, next) {
 
-  db.all("select * from user", function (err, res) {
+  db.all("select * from notebook", function (err, res) {
     if (err) console.log(err);
     else console.log(JSON.stringify(res));
   });
 
-  res.render('notebooks', { title: 'Express', aa: '<h4>aa</h4>' });
+  response.render('notebooks');
+});
+
+/* add a notebook */
+router.post('/add_nb', function (request, response, next) {
+
+  var request_body = request.body;
+  var sql = "insert into notebook('nb_name', 'nb_icon_id', 'user_id') values('" + request_body.nb_name + "', '" +
+    request_body.nb_icon_id + "', '" + request_body.user_id + "')";
+
+  db.all(sql, function (err, res) {
+    if (err) {
+      console.log(err);
+      response.render('error')
+    } else {
+      console.log(JSON.stringify(res));
+      response.send("add success");
+    }
+  });
+});
+
+/* delete a notebook */
+router.post('/delete_nb', function (request, response, next) {
+
+  var request_body = request.body;
+  var sql = "delete from notebook where nb_id = '" + request_body.nb_id + "'";
+
+  db.all(sql, function (err, res) {
+    if (err) {
+      console.log(err);
+      response.render('error')
+    } else {
+      console.log(JSON.stringify(res));
+      response.send('delete success');
+    }
+  });
 });
 
 module.exports = router;
