de685c708248b0b9e4b051762c6a9e087e0cd415
CharlesFeng <fdfengjunjie@126.com>
2017-12-09 18:15:46 +0800

    MODIFY：将笔记的编辑区域提为公有

--- public/javascripts/note_interaction.js
+++ public/javascripts/note_interaction.js
@@ -11,7 +11,7 @@ $(".note_div").click(function (e) {
         ['height', ['height']],
         ['insert', ['table', 'picture', 'fullscreen']]
       ],
-      placeholder: 'write here...',
+      placeholder: '在这里开始记录您的笔记吧！',
       height: 545,
       minHeight: null,
       maxHeight: null,
@@ -36,4 +36,22 @@ $(".fill_space").click(function (e) {
   // alert(markupStr);
   $('.mynote').summernote('destroy');
   if (e) e.stack;
+});
+
+// TODO 保存当前文档中的内容（待确定笔记本的选择）
+$('.save_now_note').click(function () {
+  var note = $(this).parent().parent();
+
+  var note_name = note.find('#note_name_input').val();
+  var note_tag = note.find('#note_tag_input').val();
+  var note_content = note.find('.mynote').html();
+});
+
+// 丢弃当前文档中的内容
+$('.discard_now_note').click(function () {
+  var note = $(this).parent().parent();
+
+  note.find('#note_name_input').val("");
+  note.find('#note_tag_input').val("");
+  note.find('.mynote').html("");
 });
