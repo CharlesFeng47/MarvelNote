5bc1e711c90965a75131ace4f524cdf8de5630ee
CharlesFeng <fdfengjunjie@126.com>
2017-12-11 01:05:14 +0800

    ADD：管理员用户管理界面

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,16 @@ var db = new sqlite.Database('./MarvelNote.sqlite');
 router.get('/', function(request, response, next) {
   console.log("GET HOME");
 
-  db.all("select * from notebook limit 1", function (err, res) {
-    if (err) {
-      console.log(err);
-      response.render('error');
-    } else {
-      console.log(JSON.stringify(res));
-      response.render('index', {has_note: false, default_nb: res});
-    }
-  });
+  // db.all("select * from notebook limit 1", function (err, res) {
+  //   if (err) {
+  //     console.log(err);
+  //     response.render('error');
+  //   } else {
+  //     console.log(JSON.stringify(res));
+  //     response.render('index', {has_note: false, default_nb: res});
+  //   }
+  // });
+  response.render('admin_index');
 
 });
 
