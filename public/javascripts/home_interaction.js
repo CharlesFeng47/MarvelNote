5df9451339da420a6df082dad5fe54c550e94b65
CharlesFeng <fdfengjunjie@126.com>
2017-12-11 16:36:24 +0800

    ADD：用户登录注册

--- public/javascripts/home_interaction.js
+++ public/javascripts/home_interaction.js
@@ -2,12 +2,59 @@
  * 注册
  */
 $('.sign_up').click(function () {
-  alert("sign");
+  var data = {
+    id: $('#input_id').val(),
+    pwd: $('#input_pwd').val()
+  };
+
+  $.ajax({
+    type: "post",
+    async: true,
+    url: "/sign_up",
+    data: data,
+
+    success: function (result) {
+      if (result === "-1") {
+        alert("该用户编号已被注册，请使用其他编号！");
+      } else {
+        alert("注册成功！");
+
+      }
+    },
+    error: function (result) {
+      alert("错误" + result);
+    }
+  });
+
 });
 
 /**
  * 登录
  */
 $('.log_in').click(function () {
-  alert("login");
+  var data = {
+    id: $('#input_id').val(),
+    pwd: $('#input_pwd').val()
+  };
+
+  $.ajax({
+    type: "post",
+    async: true,
+    url: "/log_in",
+    data: data,
+
+    success: function (result) {
+      if (result === "-1") {
+        alert("该用户编号还未被注册，请注册后再登录！");
+      } else if (result === "0") {
+        alert("登录成功！");
+      } else if (result === "1") {
+        alert("密码错误！")
+      }
+    },
+    error: function (result) {
+      alert("错误" + result);
+    }
+  });
+
 });
--- routes/home.js
+++ routes/home.js
@@ -7,19 +7,103 @@ var db = new sqlite.Database('./MarvelNote.sqlite');
 /**
  * 登录注册主界面
  */
-router.get('/', function(request, response, next) {
+router.get('/', function (request, response, next) {
   console.log("GET HOME");
+  response.render('home');
+});
 
-  db.all("select * from notebook", function (err, res) {
+/**
+ * 注册
+ * @return "-1"：已被注册
+ *          "0"：注册成功
+ */
+router.post('/sign_up', function (request, response, next) {
+
+  // 先查询所有已注册普通用户信息，比对数据库中没有后再插入
+  db.all("select * from user", function (err, res) {
     if (err) {
       console.log(err);
       response.render('error');
     } else {
       console.log(JSON.stringify(res));
-      response.render('home', {nb_data: res});
+
+      var request_body = request.body;
+      var sign_up_id = request_body.id;
+      var sign_up_pwd = request_body.pwd;
+
+      var has_been_signed_up = false;
+      for (var i = 0; i < res.length; i++) {
+        var temp_user = res[i];
+        if (temp_user.id === sign_up_id) {
+          has_been_signed_up = true;
+          response.send("-1");
+        }
+      }
+
+      if (!has_been_signed_up) {
+        // 插入数据库
+        var now_datetime = new Date().toISOString();
+        var date_time_separator = now_datetime.indexOf("T");
+        var date_string = now_datetime.substring(0, date_time_separator);
+
+        var sql = "insert into user('id', 'password', 'is_admin', 'join_date') values('" + sign_up_id + "', '" +
+          sign_up_pwd + "', '0', '" + date_string + "')";
+        console.log(sql);
+
+        db.all(sql, function (err, res) {
+          if (err) {
+            console.log(err);
+            response.render('error')
+          } else {
+            console.log(JSON.stringify(res));
+            response.send("0");
+          }
+        });
+      }
     }
   });
-
 });
+
+/**
+ * 登录
+ * @return "-1"：未被注册
+ *          "0"：已注册且密码正确，登录成功
+ *          "1"：已注册但密码不正确，登录失败
+ */
+router.post('/log_in', function (request, response, next) {
+
+  // 先查询所有用户信息
+  db.all("select * from user", function (err, res) {
+    if (err) {
+      console.log(err);
+      response.render('error');
+    } else {
+      console.log(JSON.stringify(res));
+
+      var request_body = request.body;
+      var log_in_id = request_body.id;
+      var log_in_pwd = request_body.pwd;
+
+      var has_been_signed_up = false;
+      for (var i = 0; i < res.length; i++) {
+        var temp_user = res[i];
+        if (temp_user.id === log_in_id) {
+          has_been_signed_up = true;
+          if (temp_user.password === log_in_pwd) {
+            response.send("0");
+          } else {
+            response.send("1");
+          }
+
+        }
+      }
+
+      if (!has_been_signed_up) {
+        response.send("-1");
+      }
+    }
+  });
+});
+
 
 module.exports = router;
