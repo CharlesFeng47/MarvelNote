c8e848b4d226186aa920f3756ef40f012bd7ec86
CharlesFeng <fdfengjunjie@126.com>
2017-12-12 01:30:37 +0800

    ADD：模糊搜索

--- public/javascripts/home_interaction.js
+++ public/javascripts/home_interaction.js
@@ -7,24 +7,28 @@ $('.sign_up').click(function () {
     pwd: $('#input_pwd').val()
   };
 
-  $.ajax({
-    type: "post",
-    async: true,
-    url: "/sign_up",
-    data: data,
+  if (data.id === "" || data.pwd === "") {
+    alert("账号密码填写不完整，请输入完整！");
+  } else {
+    $.ajax({
+      type: "post",
+      async: true,
+      url: "/sign_up",
+      data: data,
 
-    success: function (result) {
-      if (result === "-1") {
-        alert("该用户编号已被注册，请使用其他编号！");
-      } else {
-        alert("注册成功！");
-        window.location.href="/index";
+      success: function (result) {
+        if (result === "-1") {
+          alert("该用户编号已被注册，请使用其他编号！");
+        } else {
+          alert("注册成功！");
+          window.location.href = "/index";
+        }
+      },
+      error: function (result) {
+        alert("错误" + result);
       }
-    },
-    error: function (result) {
-      alert("错误" + result);
-    }
-  });
+    });
+  }
 });
 
 /**
@@ -36,25 +40,29 @@ $('.log_in').click(function () {
     pwd: $('#input_pwd').val()
   };
 
-  $.ajax({
-    type: "post",
-    async: true,
-    url: "/log_in",
-    data: data,
+  if (data.id === "" || data.pwd === "") {
+    alert("账号密码填写不完整，请输入完整！");
+  } else {
+    $.ajax({
+      type: "post",
+      async: true,
+      url: "/log_in",
+      data: data,
 
-    success: function (result) {
-      if (result === "-1") {
-        alert("该用户编号还未被注册，请注册后再登录！");
-      } else if (result === "0") {
-        window.location.href="/index";
-      } else if (result === "1") {
-        alert("密码错误！")
-      } else if (result === "2") {
-        window.location.href="/admin";
+      success: function (result) {
+        if (result === "-1") {
+          alert("该用户编号还未被注册，请注册后再登录！");
+        } else if (result === "0") {
+          window.location.href = "/index";
+        } else if (result === "1") {
+          alert("密码错误！")
+        } else if (result === "2") {
+          window.location.href = "/admin";
+        }
+      },
+      error: function (result) {
+        alert("错误" + result);
       }
-    },
-    error: function (result) {
-      alert("错误" + result);
-    }
-  });
+    });
+  }
 });
