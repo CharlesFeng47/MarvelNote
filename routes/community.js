d7de258bbce207e0aaf5ee32a16e4be2a2b86b58
CharlesFeng <fdfengjunjie@126.com>
2017-12-12 14:22:42 +0800

    MODIFY：恢复到之前没有实现点赞的版本

--- routes/community.js
+++ routes/community.js
@@ -4,8 +4,6 @@ var router = express.Router();
 var sqlite = require('sqlite3');
 var db = new sqlite.Database('./MarvelNote.sqlite');
 
-var async = require('async');
-
 /**
  * 访问朋友圈界面的主界面
  */
@@ -17,107 +15,64 @@ router.get('/', function (request, response, next) {
       console.log(request.query);
 
       // 所有普通用户
-      db.all("select * from user where is_admin = 0 and id != '" + request.session.cur_user + "'", function (err, all_users) {
+      db.all("select * from user where is_admin = 0 and id != '" + request.session.cur_user + "'", function (err, res) {
         if (err) {
           console.log(err);
           response.render('error');
         } else {
-          console.log("all_users: " + JSON.stringify(all_users));
+          var all_users = res;
+          console.log(JSON.stringify(all_users));
 
           // 当前用户关注的用户
-          db.all("select * from relationship where following = '" + request.session.cur_user + "'", function (err, now_following_users) {
+          db.all("select * from relationship where following = '" + request.session.cur_user + "'", function (err, res) {
             if (err) {
               console.log(err);
               response.render('error')
             } else {
-              console.log("now_following_users: " + JSON.stringify(now_following_users));
+              console.log(JSON.stringify(res));
 
-              var all_public_notes;
-              var upvote_data = [];
-              async.parallel([
-                // user_data
-                function () {
-                  var user_data = [];
-                  var user_data_index = 0;
-                  // 对所有用户在关于当前用户是否关注方面进行区分
-                  for (var i = 0; i < all_users.length; i++) {
-                    var is_following = false;
-                    for (var j = 0; j < now_following_users.length; j++) {
-                      if (all_users[i].id === now_following_users[j].be_followed) {
-                        is_following = true;
-                      }
-                    }
-
-                    user_data[user_data_index] = {
-                      user_id: all_users[i].id,
-                      is_following: is_following
-                    };
-                    user_data_index++;
+              var user_data = [];
+              var user_data_index = 0;
+              // 对所有用户在关于当前用户是否关注方面进行区分
+              for (var i = 0; i < all_users.length; i++) {
+                var is_following = false;
+                for (var j = 0; j < res.length; j++) {
+                  if (all_users[i].id === res[j].be_followed) {
+                    is_following = true;
                   }
-                  console.log("user_data: " + JSON.stringify(user_data));
                 }
-                // share_notes_data 和 upvote_data
-                , function () {
-                  async.series([
-                    // share_notes_data
-                    function () {
-                      console.log("开始串行1");
-                      var all_public_notes_sql = "select note.*, user_id from note, notebook, relationship where following = '"
-                        + request.session.cur_user + "' " + "and be_followed = notebook.user_id and notebook.nb_id = note.nb_id "
-                        + "order by update_time desc";
 
-                      async.series([
-                        function () {
-                          db.all(all_public_notes_sql, function (err, res) {
-                            if (err) {
-                              console.log(err);
-                            } else {
-                              all_public_notes = res;
-                              console.log("all_public_notes: " + JSON.stringify(all_public_notes));
-                            }
-                          });
-                          console.log("串行1.1 return");
-                        }
-                      ]);
-                      console.log("串行1 return");
-                    },
-                    // upvote_data
-                    function () {
-                      console.log("开始串行2");
+                user_data[user_data_index] = {
+                  user_id: all_users[i].id,
+                  is_following: is_following
+                };
+                user_data_index++;
+              }
 
-                      // 对每一公开的笔记进行点赞统计
-                      var share_note_index = 0;
-
-                      async.whilst(
-                        function () {
-                          return share_note_index < all_public_notes.length;
-                        },
-                        function () {
-                          console.log(JSON.stringify(all_public_notes[i]));
-                          var each_upvote_list = get_upvote_data(all_public_notes[i], request.session.cur_user);
-
-                          var has_upvoted = false;
-                          for (var j = 0; j < each_upvote_list.length; j++) {
-                            if (each_upvote_list[j] === cur_user) {
-                              has_upvoted = true;
-                            }
-                          }
-
-                          upvote_data[share_note_index] = {
-                            upvote_num: each_upvote_list.length,
-                            has_upvoted: has_upvoted
-                          };
-                          share_note_index++;
-                        },
-                        function (err) {
-                          console.log(err);
-                        }
-                      );
-                    }
-                  ], function (err, results) {
-                  });
+              var all_public_notes_sql = "select note.*, user_id from note, notebook, relationship where following = '"
+                + request.session.cur_user + "' " + "and be_followed = notebook.user_id and notebook.nb_id = note.nb_id "
+                + "order by update_time desc";
+              db.all(all_public_notes_sql, function (err, res) {
+                if (err) {
+                  console.log(err);
+                  response.render('error')
+                } else {
+                  console.log(JSON.stringify(res));
+                  if (res.length !== 0) {
+                    response.render('community', {
+                      user_data: user_data,
+                      has_share_note: true,
+                      share_notes_data: res,
+                      cur_user: request.session.cur_user
+                    });
+                  } else{
+                    response.render('community', {
+                      user_data: user_data,
+                      has_share_note: false,
+                      cur_user: request.session.cur_user
+                    });
+                  }
                 }
-              ], function (err, results) {
               });
             }
           });
@@ -159,31 +114,6 @@ router.post('/follow', function (request, response, next) {
   });
 });
 
-/**
- * 点赞／取消点赞
- */
-router.post('/upvote', function (request, response, next) {
-
-  var request_body = request.body;
-  var sql;
-  if (request_body.now_following === 'true') {
-    sql = "insert into relationship('be_followed', 'following') values('" + request_body.to_follow + "', '" + request.session.cur_user + "')";
-  } else {
-    sql = "delete from relationship where be_followed='" + request_body.to_follow + "' and following='" + request.session.cur_user + "'";
-  }
-  console.log(sql);
-
-  db.all(sql, function (err, res) {
-    if (err) {
-      console.log(err);
-      response.render('error')
-    } else {
-      console.log(JSON.stringify(res));
-      response.send("follow state change success");
-    }
-  });
-});
-
 
 /**
  * 访问一个专门的笔记条目
@@ -204,28 +134,5 @@ router.get('/:cur_note_id', function (request, response, next) {
     }
   });
 });
-
-/**
- * 根据一条分享笔记记录，获取笔记的点赞数据
- */
-function get_upvote_data(one_share_note, cur_user) {
-  var each_upvote_sql = "select * from upvote where note_id = '" + one_share_note.note_id + "'";
-
-  async.series([
-    function (cb) {
-      db.all(each_upvote_sql, function (err, each_upvote_list) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("each_upvote_list: " + JSON.stringify(each_upvote_list));
-          return each_upvote_list;
-        }
-      });
-    }
-  ], function (err, results) {
-    console.log("-----------------callback results");
-    return results;
-  });
-}
 
 module.exports = router;
